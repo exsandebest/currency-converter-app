d3871b3fd9d93dab6034d3c2fdf11008333dcfce
exsandebest <exsandebest@yandex.ru>
2020-06-04 13:55:44 +0300

    Stage 3 => Added (£)GBP, (¥)CNY, (₴)UAH

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -13,17 +13,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 var currencies: IntArray = intArrayOf(0, 1)
-val currenciesMap: Map<Int, String> = mapOf(0 to "RUB", 1 to "USD", 2 to "EUR")
+val currenciesMap: Map<Int, String> = mapOf(0 to "RUB", 1 to "USD", 2 to "EUR", 3 to "GBP", 4 to "CNY", 5 to "UAH")
 val fixedRates: Map<String, Map<String, Double>> = mapOf(
-    "RUB" to mapOf("USD" to 1.0/74.0,
-                    "RUB" to 1.0,
-                    "EUR" to 1.0/77.0),
+    "RUB" to mapOf("RUB" to 1.0,
+                    "USD" to 1.0/74.0,
+                    "EUR" to 1.0/77.0,
+                    "GBP" to 1.0/86.0,
+                    "CNY" to 1.0/10.0,
+                    "UAH" to 1.0/3.0),
     "USD" to mapOf("RUB" to 74.0,
                     "USD" to 1.0,
-                    "EUR" to 74.0/77.0),
+                    "EUR" to 74.0/77.0,
+                    "GBP" to 74.0/86.0,
+                    "CNY" to 74.0/10.0,
+                    "UAH" to 74.0/3.0),
     "EUR" to mapOf("RUB" to 77.0,
                     "USD" to 77.0/74.0,
-                    "EUR" to 1.0))
+                    "EUR" to 1.0,
+                    "GBP" to 77.0/86.0,
+                    "CNY" to 77.0/10.0,
+                    "UAH" to 77.0/3.0),
+    "GBP" to mapOf("RUB" to 86.0,
+                    "USD" to 86.0/74.0,
+                    "EUR" to 86.0/77.0,
+                    "GBP" to 1.0,
+                    "CNY" to 86.0/10.0,
+                    "UAH" to 86.0/3.0),
+    "CNY" to mapOf("RUB" to 10.0,
+                    "USD" to 10.0/74.0,
+                    "EUR" to 10.0/77.0,
+                    "GBP" to 10.0/86.0,
+                    "CNY" to 1.0,
+                    "UAH" to 10.0/3.0),
+    "UAH" to mapOf("RUB" to 3.0,
+                    "USD" to 3.0/74.0,
+                    "EUR" to 3.0/77.0,
+                    "GBP" to 3.0/86.0,
+                    "CNY" to 3.0/10.0,
+                    "UAH" to 1.0))
 
 
 class MainActivity : AppCompatActivity() {
